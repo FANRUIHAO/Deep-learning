5d5fec08107601bccafa587bace2c6fccf6d1362
FAANN <103813999+FANRUIHAO@users.noreply.github.com>
2025-02-08 15:50:37 +0800

    Update my_AlexNet.py

--- DL2神经网络项目代码/my_AlexNet.py
+++ DL2神经网络项目代码/my_AlexNet.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class vgg_layer(nn.Module):
-    def __init__(self, in_channel, out_channel):
+    def __init__(self, in_channel, out_channel):#初始化参数
         super(vgg_layer, self).__init__()
         self.conv1 = nn.Conv2d(in_channel, out_channel)#第一个卷积
         self.conv2 = nn.Conv2d(out_channel, out_channel)#第一个卷积
@@ -31,11 +31,11 @@ class MyVgg(nn.Module):
 
         self.layer5 = vgg_layer(512, 512, 512)
         self.adapool = nn.AdaptiveAvgPool2d(7)
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(25088, 4896)
+        self.relu = nn.ReLU()#relu激活函数
+        self.fc1 = nn.Linear(25088, 4896)#张量维度
 
     #VGG
-class myModel(nn.Module):
+class myModel(nn.Module):#定义模型 继承nn.Module
      def __init__(self, num_cls):
          super(myModel, self).__init__()
          self.layer1 = vgg_layer(3,64,64)
