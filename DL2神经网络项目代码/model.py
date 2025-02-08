de66f62e10f17c79fb9d9c6ca1a23ec9263381a9
FAANN <103813999+FANRUIHAO@users.noreply.github.com>
2025-02-08 15:50:34 +0800

    Update model.py

--- DL2神经网络项目代码/model.py
+++ DL2神经网络项目代码/model.py
@@ -9,11 +9,11 @@ class MyModel(nn.Module):
         self.fc2 = nn.Linear(100, 1)#第二层全链接 输出直接到一维  需要激活函数激活
 
     def forward(self, x):#模型前向过程
-        x = self.fc1(x)
-        x = self.relu1(x)
-        x = self.fc2(x)
+        x = self.fc1(x) #全链接
+        x = self.relu1(x) #激活函数
+        x = self.fc2(x) #全链接
 
-        #会导致两维的x，所以要变成一维的
+        #会导致两维的x，难以转换为概率，所以需要将其转换为一维
         if len(x.size()) > 1:
             return x.squeeze(1)#去掉一维的
 
